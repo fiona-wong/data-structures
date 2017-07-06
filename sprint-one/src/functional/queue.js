39d5887461ac064d7e55e698d1837426b342b9cd
Hack Reactor Student <student@hackreactor.com>
2017-07-05 20:51:28 -0700

    finished functional shared

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,23 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  
   someInstance.enqueue = function(value) {
+    //adds item to the back of the queue
+    storage[Object.keys(storage).length] = value;
   };
 
   someInstance.dequeue = function() {
+    //removes item from the front of the queue
+    var frontOfStack = storage[Object.keys(storage).length - 1];
+    delete storage[0];
+    return frontOfStack;
+    
+
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
