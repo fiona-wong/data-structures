860e585fd0b8435d511b4dce4a07363ccbfa6390
Hack Reactor Student <student@hackreactor.com>
2017-07-08 11:42:35 -0700

    missing depthFirstLog

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,61 @@
 var BinarySearchTree = function(value) {
+  var tree = {};
+  
+  tree.value = value;
+  
+  tree.left = null;
+  
+  tree.right = null;
+
+  tree.insert = function(val) {
+    var inserted = BinarySearchTree(val);
+    //check if value passed into insert is lower than tree value
+      //if it is lower check if left is null or contains value
+    //if left contains value check if val is lower or higher
+     //if left va
+    if (inserted.value > tree.value) { 
+      if (tree.right === null) {
+        tree.right = inserted;
+      } else {
+        tree.right.insert(val);
+      }
+    } else if (inserted.value < tree.value) {
+      if (tree.left === null) {
+        tree.left = inserted;
+      } else {
+        tree.left.insert(val);
+      }
+    }
+    
+  };
+
+  tree.contains = function(val) {
+    if (tree.value === val) {
+      return true;
+    } else if (val < tree.value && tree.left !== null) {
+      return tree.left.contains(val);
+    } else if (val > tree.value && tree.right !== null) {
+      return tree.right.contains(val);
+    } else {
+      return false;
+    }
+    
+  };
+  
+  tree.depthFirstLog = function(cb) {
+    if (tree.value !== null) {
+      cb(tree.value);
+    } else if (tree.left.value !== null) {
+      cb(tree.left.value);
+      tree.left.depthFirstLog(cb);
+    } else if (tree.right.value !== null) {
+      cb(tree.right.value);
+      tree.right.depthFirstLog(cb);
+    }
+  };
+
+
+  return tree;
 };
 
 
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,49 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tupole = [k, v];
+  var bucket = [this._storage.get(index)];
+  if (!this._storage.get(index) || this._storage.get(index)[0] === k) {
+    this._storage.set(index, tupole);
+  } else {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][1] = v;
+      } else {
+        bucket.push(tupole);
+      }
+    }
+    this._storage.set(index, bucket);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (bucket[0] === k) {
+    return bucket[1];
+  }
+  
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (bucket[0] === k) {
+    bucket.splice(0);
+  } else {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket.splice(i, 1);
+      }  
+    }
+  }
+  this._storage.set(index, bucket);
 };
 
 
